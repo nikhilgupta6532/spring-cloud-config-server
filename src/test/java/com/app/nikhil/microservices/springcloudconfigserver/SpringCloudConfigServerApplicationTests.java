a03e1abeb69caf6a216b7446c7a3980ccb89fd2a
nikhil <NIKHIL.GUPTA@PEARSON.COM>
2020-01-14 10:23:16 +0530

    spring-cloud-config-server setup

---
+++ src/test/java/com/app/nikhil/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nikhil.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
